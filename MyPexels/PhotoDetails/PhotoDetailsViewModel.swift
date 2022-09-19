b49b739476be3d9350ba2872669667b01f27f819
ArtemPavlov72 <pavlov.tema@gmail.com>
2022-09-19 22:55:45 +0500

    PhotoDetailsViewModel updated.

--- MyPexels/PhotoDetails/PhotoDetailsViewModel.swift
+++ MyPexels/PhotoDetails/PhotoDetailsViewModel.swift
@@ -6,3 +6,22 @@
 //
 
 import Foundation
+
+protocol PhotoDetailsViewModelProtocol {
+    var photogtapherNameLabel: String { get }
+    init(photo: Photo)
+}
+
+class PhotoDetailsViewModel: PhotoDetailsViewModelProtocol {
+    var photogtapherNameLabel: String {
+        photo.photographer ?? ""
+    }
+    
+    private let photo: Photo
+    
+    required init(photo: Photo) {
+        self.photo = photo
+    }
+    
+    
+}
